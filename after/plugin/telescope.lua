59f4b696a5b7997253479b6174f0ef45d663d168
Charlie Wills <cwills0114@gmail.com>
2023-11-08 22:32:32 +0000

    Add grep using visual mode selected word

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('v', '<C-f>', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', default_opts)
