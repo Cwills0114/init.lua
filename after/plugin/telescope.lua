428a172e0f05d4a64287e250c94f38020292f3ec
Charlie Wills <cwills0114@gmail.com>
2023-11-11 00:42:12 +0000

    Update find files (Git only) key to <leader>pp

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('v', '<leader>ps', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', default_opts)
+
